ad72697805f25b7a5c151f6adcd9eb62c2cf1e52
XGM <xgm-skywave@users.noreply.github.com>
2018-06-04 21:46:10 +0800

    添加redis的测试用例

---
+++ test/redis_test.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "test_common.h"
+
+#include "xdb/redis/RedisClient.h"
+
+#include <glog/logging.h>
+
+#include <memory>
+
+#ifdef OPEN_REDIS_CLIENT
+//------------------------------------------------------------------------
+// 测试Redis连接
+//------------------------------------------------------------------------
+TEST_UNIT(TestRedisConnect)
+{
+	RedisClient::InitInstance();
+	RedisClient::Me()->Init("127.0.0.1:6379", "", "");
+	H_EXPECT_TRUE(RedisClient::Me()->Start());
+	RedisClient::Me()->Stop();
+	RedisClient::DestroyInstance();
+}
+#endif
