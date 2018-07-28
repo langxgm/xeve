db5a7c4e51b3b4f18a1c79050c5e4589bea57e84
XGM <xgm-skywave@users.noreply.github.com>
2018-07-28 20:33:42 +0800

    添加生成序列号函数

---
+++ xshare/net/GenSN.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "GenSN.h"
+
+#include "xbase/TimeUtil.h"
+
+#include <atomic>
+
+std::string GenSN::T20()
+{
+	// 生成20个字符的流水号
+	static std::atomic_uint sn_num = { 0 };
+	uint64_t nNow = TimeUtil::GetCurrentTimeMillis();
+
+	char szSN[32];
+	sprintf(szSN, "%08X%08X%04d", uint32_t(nNow >> 32) & 0xFFFFFFFF, uint32_t(nNow), (++sn_num) % 10000);
+	return szSN;
+}
