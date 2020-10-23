e88153d077d9adb790fdddfe10f1edb41fcbf7f2
XGM <252040903@qq.com>
2020-10-23 14:32:40 +0800

    Refactor (xbase) 负载平均值模板

--- xbase/LoadAverage.h
+++ xbase/LoadAverage.h
@@ -8,7 +8,7 @@
 //------------------------------------------------------------------------
 #pragma once
 
-#include <list>
+#include <vector>
 
 template<class _Value_t>
 class LoadAverage
@@ -18,8 +18,11 @@ public:
 public:
 	LoadAverage(uint32_t nMaxSize)
 		: m_nMaxSize(nMaxSize)
-		, m_nAccumulate(0)
+		, m_nBeginIndex(0)
+		, m_nEndIndex(0)
+		, m_nSum(0)
 	{
+		m_vecValues.reserve(nMaxSize);
 	}
 
 	//------------------------------------------------------------------------
@@ -27,33 +30,54 @@ public:
 	//------------------------------------------------------------------------
 	void PushValue(const value_type& v)
 	{
-		if (m_listValues.size() == m_nMaxSize)
+		if (m_vecValues.size() == m_nMaxSize)
 		{
-			m_nAccumulate -= m_listValues.back();
-			m_listValues.pop_back();
+			m_nSum -= m_vecValues[m_nBeginIndex];
+			m_vecValues[m_nBeginIndex] = v;
+			m_nSum += v;
+			m_nBeginIndex = (++m_nBeginIndex % m_nMaxSize);
+			m_nEndIndex = (++m_nEndIndex % m_nMaxSize);
 		}
-
-		m_nAccumulate += v;
-		m_listValues.push_front(v);
+		else
+		{
+			m_nSum += v;
+			m_vecValues.push_back(v);
+			m_nBeginIndex = 0;
+			m_nEndIndex = m_vecValues.size() - 1;
+		}
 	}
 
 	//------------------------------------------------------------------------
 	// 获得平均值
 	//------------------------------------------------------------------------
-	value_type GetAverageValue()
+	value_type GetAverageValue() const
 	{
-		if (m_listValues.size() == 0)
+		if (m_vecValues.size() == 0)
 		{
 			return value_type(0);
 		}
-		return m_nAccumulate / m_listValues.size();
+		return m_nSum / m_vecValues.size();
 	}
+
+	//------------------------------------------------------------------------
+	// 获得总数
+	//------------------------------------------------------------------------
+	value_type GetSum() const { return m_nSum; }
+
+	//------------------------------------------------------------------------
+	// 获得列表长度
+	//------------------------------------------------------------------------
+	uint32_t GetSize() const { return m_vecValues.size(); }
+	uint32_t GetMaxSize() const { return m_nMaxSize; }
 
 private:
 	// 数量限制
 	uint32_t m_nMaxSize;
 	// 数据列表
-	std::list<value_type> m_listValues;
-	// 累积
-	value_type m_nAccumulate;
+	std::vector<value_type> m_vecValues;
+	// 头尾下标
+	uint32_t m_nBeginIndex;
+	uint32_t m_nEndIndex;
+	// 求和总数
+	value_type m_nSum;
 };
